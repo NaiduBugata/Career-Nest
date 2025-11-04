c1846759bbb91207618c6d38df57eb01ef5f6847
NaiduBugata <naidubugata88@gmail.com>
2025-11-04 11:45:31 +0530

    Use HashRouter so hash URLs (/#/help) work locally and on static hosts

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Landing from "./pages/Landing";
 import AuthForm from "./pages/AuthForm";
 import RoleSelection from "./pages/Role";
